c5278741dfae493cbc960a81be8d4de0a126900a
Bgoczan <32751776+Bgoczan@users.noreply.github.com>
2022-02-03 14:58:43 +0100

    Directory generation added

--- Directrory_Structure/placeholder_creator.bat
+++ Directrory_Structure/placeholder_creator.bat
@@ -1,1 +1,23 @@
+md Media\
+md MissionPlanning\AIV_Plan
+md MissionPlanning\Checklists
+md MissionPlanning\GasCylinder
+md MissionPlanning\LaunchNotice
+md MissionPlanning\MissionParameters
+md MissionPlanning\Predictions
+md MissionPlanning\Staff
+md PostFlightAnalysis\Analysis
+md PostFlightAnalysis\FlightReport
+md PostFlightAnalysis\GND_LOG
+md PostFlightAnalysis\KML
+md PostFlightAnalysis\LessonsLearned
+md PostFlightAnalysis\OBC_LOG
+md PostFlightAnalysis\TOMI_LOG
+md PreFlightTesting\TestReports
+md RemoteSensing\GOPRO1
+md RemoteSensing\UPRACAM
+md RemoteSensing\GOPRO7
+md RemoteSensing\SJCAM
+md RemoteSensing\NEXTBASE
+
 for /r "%cd%" %%i in (.) do @copy "%cd%\_placeholder.txt" "%%i"
